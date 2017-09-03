a1ce0cbba649b9ac7a6a9680972dcc35d7a23451
Corinna Vinschen <xda@vinschen.de>
2017-09-03 13:24:04 +0200

    lentislte: init: move property_override to apq8084-common
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- init/init_lentislte.cpp
+++ init/init_lentislte.cpp
@@ -62,17 +62,6 @@ const char *device[3] =  {
   "lentisltelgt"
 };
 
-static void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
 void init_target_properties()
 {
     std::string platform = property_get("ro.board.platform");
